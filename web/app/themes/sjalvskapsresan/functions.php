a8fa3a17519e3b72d288526e601c4c52df40fd67
Stefan Ledin <stefan@Stefans-iMac.lan>
2018-06-18 06:27:51 +0200

    Grunden lagd

---
+++ web/app/themes/sjalvskapsresan/functions.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Timber
+ */
+require_once($root_dir . '/vendor/autoload.php');
+$timber = new \Timber\Timber();
+
+/**
+ * Content
+ *
+ * 1. Custom post types
+ * 2. Enqueue scripts and styles
+ * 3. Images
+ * 4. Filters
+ * 5. Actions
+ * 6. Menus
+ * 7. Helpers
+ */
+require 'includes/custom-post-types.php';
+
+require 'includes/enqueue-scripts-styles.php';
+
+require 'includes/images/images.php';
+require 'includes/images/custom-image-sizes.php';
+
+require 'includes/filters.php';
+
+require 'includes/actions.php';
+
+require 'includes/menus.php';
+
+require 'includes/helpers.php';
