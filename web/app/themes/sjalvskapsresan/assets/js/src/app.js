54909c8751f6b6a9c1be88cb151a870892f56a1e
Stefan Ledin <stefan.ledin@gmail.com>
2018-06-25 14:45:39 +0200

    Knapp för att öppna sidebar

--- web/app/themes/sjalvskapsresan/assets/js/src/app.js
+++ web/app/themes/sjalvskapsresan/assets/js/src/app.js
@@ -4,8 +4,13 @@ document.addEventListener('DOMContentLoaded', (event) => {
     });
 });
 
-(function () {
-    
-    //new SidebarNav();
-})();
-
+const opensidebarButton = document.querySelector('button.js-open-sidebar');
+const overlay = document.querySelector('div.overlay');
+const sidebar = document.querySelector('aside.sidebar');
+const openSidebar = (event) => {
+    event.preventDefault();
+    document.body.classList.toggle('sidebar-is-open');
+    sidebar.scrollIntoView();
+}
+opensidebarButton.addEventListener('click', openSidebar);
+overlay.addEventListener('click', openSidebar);
