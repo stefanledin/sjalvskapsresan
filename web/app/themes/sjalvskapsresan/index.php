73eeb114269648af3e9e69af892538eb63e1d764
Stefan Ledin <stefan@Stefans-iMac.lan>
2018-06-30 12:05:34 +0200

    Jobbar på (ned)räkningen

--- web/app/themes/sjalvskapsresan/index.php
+++ web/app/themes/sjalvskapsresan/index.php
@@ -1,10 +1,18 @@
 <?php
-$context = Timber::get_context();
+use Carbon\Carbon;
 
-$trip_start = get_field('trip_start', 'option');
+$context = Timber::get_context();
+date_default_timezone_set('Europe/Stockholm');
+
+$trip_start = strtotime( get_field('trip_start', 'option') );
 $trip_end  = get_field('trip_end', 'option');
-$context['trip_has_started'] = time() < strtotime( $trip_start );
-$context['trip_timer'] = $trip_start;
+$now = new Carbon();
+$context['trip_has_started'] = ( $now->timestamp < strtotime( $trip_start ) );
+if ( ! $context['trip_has_started'] ) {
+    $trip_start = Carbon::createFromTimestamp( $trip_start );
+    $countdown = $now->diff( $trip_start );
+    $context['countdown'] = sprintf('T-minus: <time>%s/%s:%s:%s</time>', $countdown->d, $countdown->h, $countdown->i, $countdown->s);
+}
 $context['timeline'] = get_field('timeline', 'options');
 
 Timber::render( 'index.twig', $context );
